73879af35b42aca1cd859526edc24aa40a162aad
Adi Poluri <52992596+adipoluri@users.noreply.github.com>
2023-09-23 03:07:59 -0700

    Test (#1)
    * Update page.tsx
    
    * init api

--- app/selection/page.tsx
+++ app/selection/page.tsx
@@ -30,6 +30,8 @@ export default function Page() {
         </div>
       </div>
 
+      
+
       <View className='absolute top-0 flex h-screen w-full flex-col items-center justify-center'>
         <Blob />
         <Common />
