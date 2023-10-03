c80dba555e51b658a628137d738aff340cab7e0d
Adi Poluri <adipoluri@gmail.com>
2023-10-02 17:21:38 -0700

    merged auth routes to v1

--- app/auth/v1/callback/route.ts
+++ app/auth/v1/callback/route.ts
@@ -10,7 +10,6 @@ export async function GET(request: Request) {
   // https://supabase.com/docs/guides/auth/auth-helpers/nextjs#managing-sign-in-with-code-exchange
   const requestUrl = new URL(request.url)
   const code = requestUrl.searchParams.get('code')
-
   if (code) {
     const supabase = createRouteHandlerClient({ cookies })
     await supabase.auth.exchangeCodeForSession(code)
