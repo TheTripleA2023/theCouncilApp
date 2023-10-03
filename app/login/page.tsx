bee869dad7a61f80972838d1c0d197a5b989d67d
Adi Poluri <adipoluri@gmail.com>
2023-10-02 17:22:03 -0700

    added new login logic

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,25 +1,34 @@
 'use client'
-
-import dynamic from 'next/dynamic'
 import { Auth } from '@supabase/auth-ui-react'
 import { createClientComponentClient } from '@supabase/auth-helpers-nextjs'
 import { ThemeSupa } from '@supabase/auth-ui-shared'
+import { useEffect, useState } from 'react'
 
 const supabase = createClientComponentClient();
 
 export default function Page() {
+  const [callback, setCallback] = useState("");
+
+  useEffect(function mount() {
+    setCallback(window.location.origin+"/auth/v1/callback")
+  });
+
   return (
     <>
-      <div className='mx-auto flex w-full flex-col flex-wrap items-center md:flex-row  lg:w-4/5'>
-        <div className='flex w-full flex-col items-start justify-center p-12 text-center md:w-2/5 md:text-left'>
-          <h1 className='my-4 text-5xl font-bold leading-tight'>We are team Triple A</h1>
-          <p className='mb-8 text-2xl leading-normal'>Hi!</p>
-          <Auth
-            supabaseClient={supabase}
-            providers={['google']}
-            onlyThirdPartyProviders
-            appearance={{ theme: ThemeSupa }}
-          />
+      <div className='root'>
+        <div className='mx-auto flex w-full flex-col flex-wrap items-center md:flex-row  lg:w-4/5'>
+          <div className='flex w-full flex-col items-start justify-center p-12 text-center md:w-2/5 md:text-left'>
+            <h1 className='my-4 text-5xl font-bold leading-tight'>We are team Triple A</h1>
+            <p className='mb-8 text-2xl leading-normal'>Hi!</p>
+            <Auth
+              supabaseClient={supabase}
+              providers={['google','github']}
+              onlyThirdPartyProviders
+              appearance={{ theme: ThemeSupa }}
+              theme='dark'
+              redirectTo={callback}
+              />
+          </div>
         </div>
       </div>
     </>
--- middleware.ts
+++ middleware.ts
@@ -6,6 +6,17 @@ import type { NextRequest } from 'next/server'
 export async function middleware(req: NextRequest) {
   const res = NextResponse.next()
   const supabase = createMiddlewareClient({ req, res })
-  await supabase.auth.getSession()
+
+  const { data: { session }} = await supabase.auth.getSession()
+  
+  if (!session) {
+    // this is a protected route - only users who are signed in can view this route
+    const url = req.nextUrl.clone()   
+    if (url.pathname === '/'|| url.pathname === "/about" || url.pathname === "/finale" || url.pathname === "/selection") {
+      url.pathname = '/login'
+      return NextResponse.redirect(url)   
+    } 
+  }
+
   return res
 }
