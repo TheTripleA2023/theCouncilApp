50bc8de1b71f8e4d048f811d336c832174703277
Taryn Wou <75547547+tarynwou@users.noreply.github.com>
2023-10-07 00:55:22 -0700

    hero component created

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,29 +1,33 @@
-import { Layout } from '@/components/dom/Layout'
-import { ChakraProvider } from '@chakra-ui/react'
-import '@/global.css'
-import { Providers } from '@/components/dom/Providers'
+import { Layout } from "@/components/dom/Layout";
+import { ChakraProvider } from "@chakra-ui/react";
+import "@/global.css";
+import { Providers } from "@/components/dom/Providers";
 
 export const metadata = {
-  title: 'The Council',
-  description: '',
-}
+	title: "The Council",
+	description: "",
+};
 
 export default function RootLayout({ children }) {
-  return (
-    <html lang='en' className='antialiased'>
-      {/*
+	return (
+		<html lang="en" className="antialiased">
+			{/*
         <head /> will contain the components returned by the nearest parent
         head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
-      <head />
-      <body>
-        {/* To avoid FOUT with styled-components wrap Layout with StyledComponentsRegistry https://beta.nextjs.org/docs/styling/css-in-js#styled-components */}
-        <Layout>        
-          <Providers>
-            {children}
-          </Providers>
-        </Layout>
-      </body>
-    </html>
-  )
+			<head>
+				<link rel="preconnect" href="https://fonts.gstatic.com" />
+				<link
+					href="https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:wght@400;700&display=swap"
+					rel="stylesheet"
+				/>
+			</head>
+			<body>
+				{/* To avoid FOUT with styled-components wrap Layout with StyledComponentsRegistry https://beta.nextjs.org/docs/styling/css-in-js#styled-components */}
+				<Layout>
+					<Providers>{children}</Providers>
+				</Layout>
+			</body>
+		</html>
+	);
 }
--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,36 +1,35 @@
-'use client'
-import { Auth } from '@supabase/auth-ui-react'
-import { createClientComponentClient } from '@supabase/auth-helpers-nextjs'
-import { ThemeSupa } from '@supabase/auth-ui-shared'
-import { useEffect, useState } from 'react'
+"use client";
+import { Auth } from "@supabase/auth-ui-react";
+import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
+import { ThemeSupa } from "@supabase/auth-ui-shared";
+import { useEffect, useState } from "react";
+import HeroComponent from "@/components/landingpage/HeroComponent";
 
 const supabase = createClientComponentClient();
 
 export default function Page() {
-  const [callback, setCallback] = useState("");
+	const [callback, setCallback] = useState("");
 
-  useEffect(function mount() {
-    setCallback(window.location.origin+"/auth/v1/callback")
-  });
+	useEffect(function mount() {
+		setCallback(window.location.origin + "/auth/v1/callback");
+	});
 
-  return (
-    <>
-      <div className='root'>
-        <div className='mx-auto flex w-full flex-col flex-wrap items-center md:flex-row  lg:w-4/5'>
-          <div className='flex w-full flex-col items-start justify-center p-12 text-center md:w-2/5 md:text-left'>
-            <h1 className='my-4 text-5xl font-bold leading-tight'>We are team Triple A</h1>
-            <p className='mb-8 text-2xl leading-normal'>Hi!</p>
-            <Auth
-              supabaseClient={supabase}
-              providers={['google','github']}
-              onlyThirdPartyProviders
-              appearance={{ theme: ThemeSupa }}
-              theme='dark'
-              redirectTo={callback}
-              />
-          </div>
-        </div>
-      </div>
-    </>
-  )
+	return (
+		<>
+			<div className="root">
+				{/* <div className="mx-auto flex w-full flex-col flex-wrap items-center md:flex-row  lg:w-4/5"> */}
+				<div>
+					<HeroComponent />
+					<Auth
+						supabaseClient={supabase}
+						providers={["google", "github"]}
+						onlyThirdPartyProviders
+						appearance={{ theme: ThemeSupa }}
+						theme="dark"
+						redirectTo={callback}
+					/>
+				</div>
+			</div>
+		</>
+	);
 }
