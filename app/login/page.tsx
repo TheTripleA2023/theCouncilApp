6283c28e70d6a38e6b383a6544096d59c9258f77
Taryn Wou <75547547+tarynwou@users.noreply.github.com>
2023-10-07 23:48:06 -0700

    drafted about the council component of landing page

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,14 +1,14 @@
 "use client";
 import HeroComponent from "@/components/landingpage/HeroComponent";
-import { Auth } from '@supabase/auth-ui-react'
-import { createClientComponentClient } from '@supabase/auth-helpers-nextjs'
-import { ThemeSupa } from '@supabase/auth-ui-shared'
-import { useEffect, useState } from 'react'
-import UpperScroll from '@/components/UpperScroll'
-import LowerScroll from '@/components/LowerScroll'
-import UsageDisclaimer from '@/components/UsageDisclaimer'
-import Footer from '@/components/Footer'
-
+import AboutCouncilComponent from "@/components/landingpage/AboutCouncilComponent";
+import { Auth } from "@supabase/auth-ui-react";
+import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
+import { ThemeSupa } from "@supabase/auth-ui-shared";
+import { useEffect, useState } from "react";
+import UpperScroll from "@/components/UpperScroll";
+import LowerScroll from "@/components/LowerScroll";
+import UsageDisclaimer from "@/components/UsageDisclaimer";
+import Footer from "@/components/Footer";
 
 const supabase = createClientComponentClient();
 
@@ -19,13 +19,13 @@ export default function Page() {
 		setCallback(window.location.origin + "/auth/v1/callback");
 	});
 
-
-  return (
-    <>
-      <div className='root'>
-        {/* <div className="mx-auto flex w-full flex-col flex-wrap items-center md:flex-row  lg:w-4/5"> */}
+	return (
+		<>
+			<div className="root">
+				{/* <div className="mx-auto flex w-full flex-col flex-wrap items-center md:flex-row  lg:w-4/5"> */}
 				<div>
 					<HeroComponent />
+
 					<Auth
 						supabaseClient={supabase}
 						providers={["google", "github"]}
@@ -34,12 +34,14 @@ export default function Page() {
 						theme="dark"
 						redirectTo={callback}
 					/>
+					<UpperScroll />
+					<AboutCouncilComponent />
 				</div>
-        <UpperScroll/>
-        <LowerScroll/>
-        <UsageDisclaimer/>
-        <Footer/>
-      </div>
-    </>
-  )
+
+				<LowerScroll />
+				<UsageDisclaimer />
+				<Footer />
+			</div>
+		</>
+	);
 }
---
+++ src/components/landingpage/AboutCouncilComponent.tsx
@@ -0,0 +1,54 @@
+import { Image, Text, Button } from "@chakra-ui/react"; // Import necessary Chakra UI components
+
+function AboutCouncilComponent(props) {
+	return (
+		<div className="about-council-component">
+			<div className="about-council-content">
+				<div className="about-council-div">
+					<Text className="about-council-h2">
+						What's the Council?
+					</Text>
+					<Text className="about-council-p1">
+						We have to make a lot of decisions, all the time-
+						whether it's choosing your next hackathon project idea,
+						texting your ex or not, or settling a debate. Sometimes,
+						you need the cold hard truth. Sometimes, you need
+						someone to feed into your delusions. But sometimes, you
+						need both!
+					</Text>
+				</div>
+
+				<div className="how-it-works-div">
+					<Image
+						className="how-it-works-image"
+						src="/img/how-it-works.png"
+					/>
+					<Text className="about-council-p2">
+						Give the Council your problem, and it'll answer with
+						four AI-generated perspectives! With 10 different
+						personalities to choose from, you can get a bunch of
+						(imaginary) friends to weigh in on your dilemmas, even
+						if you're all alone!
+					</Text>
+				</div>
+			</div>
+			<div className="how-we-started-div">
+				<div>
+					<Text className="about-council-h2">How we started</Text>
+					<Text className="about-council-p3">
+						The Council was born out of a hackathon project! It was
+						a top 12 winning project at Hack the North 2023. With
+						fruit snacks, power naps, and caffeinated chocolates, we
+						were able to build an MVP in just 36 hours. Thanks to
+						the amazing reception and feedback from the judges, we
+						decided to bring the Council to life!
+					</Text>
+				</div>
+				<Image className="how-we-started-image" />
+				<Button>Watch our demo at HTN 2023</Button>
+			</div>
+		</div>
+	);
+}
+
+export default AboutCouncilComponent;
