e55c61c888f8abe8eea0af9b599f0773ccae7d44
Adi Poluri <adipoluri@gmail.com>
2023-10-08 02:23:56 -0700

    added favicon to head

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import '@/global.css'
 import { Providers } from '@/components/dom/Providers'
 import Background from '@/components/canvas/Background'
 import Navbar from '@/components/Navbar/Navbar';
+import Head from './head';
 
 export const metadata = {
 	title: "The Council",
@@ -22,6 +23,7 @@ export default function RootLayout({ children }) {
 					href="https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:wght@400;700&display=swap"
 					rel="stylesheet"
 				/>
+        <Head/>
 			</head>
       <body>
         {/* To avoid FOUT with styled-components wrap Layout with StyledComponentsRegistry https://beta.nextjs.org/docs/styling/css-in-js#styled-components */}
