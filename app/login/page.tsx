e2438eab44bce60fa57e704cff79f28d1e8be199
Taryn Wou <75547547+tarynwou@users.noreply.github.com>
2023-10-08 20:14:43 -0700

    spacing for hero

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -24,14 +24,14 @@ export default function Page() {
 	return (
 		<>
 			<div className="root">
-				{/* <Auth
+				<Auth
 					supabaseClient={supabase}
 					providers={["google", "github"]}
 					onlyThirdPartyProviders
 					appearance={{ theme: ThemeSupa }}
 					theme="dark"
 					redirectTo={callback}
-				/> */}
+				/>
 				<HeroComponent />
 
 				<UpperScroll />
