7c455788402d195bdbd7c6ae7bcd66bec81813a7
Adi Poluri <52992596+adipoluri@users.noreply.github.com>
2023-10-22 00:10:20 -0700

    fixed finale formatting

--- app/finale/page.tsx
+++ app/finale/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { Text, Button, Image } from "@chakra-ui/react";
+import { Text, Button, Image, Stack } from "@chakra-ui/react";
 
 // import DiscoGif from "./../../public/img/Disco1.gif"
 import Link from "next/link";
@@ -13,22 +13,25 @@ export default function Page() {
 				<div className="disco-container">
 					<Image className="disco-gif" src="./img/Disco1.gif" />
 				</div>
-				<Text className="end-h1">The Council thanks you!</Text>
-				<Text className="end-h2">
-					Thank you for choosing
-					<span className="endpage-gradient-text">The Council</span>.
-				</Text>
+				<Stack alignItems={'center'} alignContent={'center'} marginTop={['100px','200px']}>
+					<Text fontSize={['32px','64px']} fontWeight={900}>The Council has been adjourned.</Text>
+					<Text className="end-h2">
+						Thank you for choosing
+						<span className="endpage-gradient-text">The Council</span>.
+					</Text>
 
-				<Text className="end-h2">We hope our advice helped.</Text>
-				<Link href={"/"}>
-					<Button
-						className="refresh-button"
-						id="refresh"
-						// onClick={refreshPage}
-					>
-						Ask another question
-					</Button>
-				</Link>
+					<Text className="end-h2">We hope our advice helped.</Text>
+					<Link href={"/"}>
+						<Button
+							className="refresh-button"
+							id="refresh"
+							// onClick={refreshPage}
+						>
+							Ask another question
+						</Button>
+					</Link>
+				</Stack>
+				
 
 				<Image className="pink-floor" src="./img/Pinkfloor.svg" />
 			</div>
